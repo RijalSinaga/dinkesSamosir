e275a0cadc0c5ed647948da3863bbc36f59a51f9
ArcherDeveloper <archerdeveloper16@gmail.com>
2019-12-12 16:15:52 +0700

    jjj

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -79,7 +79,7 @@
 <div class="modal-dialog" role="document">
     <div class="modal-content">
     <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle"><span><h1><b>Input Data Siswa</b></h1></span></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">&times;</span>
         </button>
@@ -88,7 +88,7 @@
         <form action="/siswa/create" method="POST" enctype="multipart/form-data">
         {{ csrf_field() }}
             <div class="form-group {{$errors->has('nama_depan') ? ' has-error' : ''}}">
-                <label for="exampleInputEmail1">Nama Depan</label>
+                {{-- <label for="exampleInputEmail1">Nama Depan</label> --}}
                 <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Depan" value="{{old('nama_depan')}}">
                     @if($errors->has('nama_depan'))
                         <span class="help-block">{{$errors->has('nama_depan')}}</span>
@@ -96,7 +96,7 @@
             </div>
 
             <div class="form-group {{$errors->has('nama_belakang') ? ' has-error' : ''}}">
-                <label for="exampleInputEmail1">Nama Belakang</label>
+                {{-- <label for="exampleInputEmail1">Nama Belakang</label> --}}
                 <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Belakang" value="{{old('nama_belakang')}}">
                 @if($errors->has('nama_depan'))
                     <span class="help-block">{{$errors->has('nama_belakang')}}</span>
@@ -104,7 +104,7 @@
             </div>
 
             <div class="form-group {{$errors->has('email') ? ' has-error' : ''}}">
-                <label for="exampleInputEmail1">Email</label>
+                {{-- <label for="exampleInputEmail1">Email</label> --}}
                 <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" value="{{old('email')}}">
                 @if($errors->has('nama_depan'))
                     <span class="help-block">{{$errors->has('email')}}</span>
@@ -112,7 +112,7 @@
             </div>
 
             <div class="form-group {{$errors->has('jenis_kelamin') ? ' has-error' : ''}}">
-                <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+                {{-- <label for="exampleFormControlSelect1">Jenis Kelamin</label> --}}
                 <select name="jenis_kelamin" type="text" class="form-control" id="exampleFormControlSelect1">
                     <option value="#">--Pilih Jenis Kelamin--</option>
                     <option value="L" {{(old('jenis_kelamin') == 'L') ? ' selected' : ''}}>Laki-laki</option>
@@ -121,7 +121,7 @@
             </div>
 
             <div class="form-group {{$errors->has('agama') ? ' has-error' : ''}}">
-                <label for="exampleFormControlSelect1">Agama</label>
+                {{-- <label for="exampleFormControlSelect1">Agama</label> --}}
                 <select name="agama" type="text" class="form-control" id="exampleFormControlSelect1">
                     <option value="#">--Pilih Agama--</option>
                     <option value="Islam" {{(old('agama') == 'Islam') ? ' selected' : ''}}>Islam</option>
@@ -133,12 +133,12 @@
             </div>
 
             <div class="form-group {{$errors->has('alamat') ? ' has-error' : ''}}">
-                <label for="exampleFormControlTextarea1">Alamat</label>
-                <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3">{{old('alamat')}}</textarea>
+                {{-- <label for="exampleFormControlTextarea1">Alamat</label> --}}
+                <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" placeholder="Alamat" rows="2">{{old('alamat')}}</textarea>
             </div>
 
             <div class="form-group {{$errors->has('avatar') ? ' has-error' : ''}}">
-                <label for="exampleFormControlTextarea1">Avatar</label>
+                {{-- <label for="exampleFormControlTextarea1">Avatar</label> --}}
                 <input type="file" name="avatar" class="form-control">
                 @if($errors->has('nama_depan'))
                         <span class="help-block">{{$errors->has('avatar')}}</span>
